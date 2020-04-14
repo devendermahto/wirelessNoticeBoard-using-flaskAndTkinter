4b4c839fe18f496b9301232756cdac2d6a6c576f
Pravesh Bawangade <pptechno52@gmail.com>
2020-04-14 23:09:15 +0530

    added text display feeature

--- raspberryPi/app.py
+++ raspberryPi/app.py
@@ -1,4 +1,5 @@
 from flask import *
+import cv2
 
 app = Flask(__name__)
 
@@ -9,11 +10,22 @@ def upload():
 
 
 @app.route('/image', methods=['POST'])
-def success():
+def success_image():
     if request.method == 'POST':
         f = request.files['file']
         f.save("received.png")
         return render_template("success.html", name=f.filename)
+
+
+@app.route('/text', methods=['POST'])
+def success_text():
+    if request.method == 'POST':
+        t = request.form.get('text')
+        back = cv2.imread("back.png")
+        cv2.putText(back, text=t, org=(int(back.shape[1]/4), int(back.shape[0]/2)), fontFace = cv2.FONT_HERSHEY_SIMPLEX,
+                    fontScale=1,color=(0,0,0),thickness=2)
+        cv2.imwrite("received.png", back)
+        return render_template("success.html", name="text")
 
 
 if __name__ == '__main__':
